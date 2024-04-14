5f9d075d78b2a6bef35b208ca5d5038dde741416
jalew188 <jalew188@gmail.com>
2024-04-14 10:51:51 +0200

    Modify from Lei Sun lab's template

---
+++ javascript/public.js
@@ -0,0 +1,77 @@
+window.onload = function(){changeHeaderNav()}
+window.onresize = function(){changeHeaderNav()}
+
+function changeHeaderNav() {
+
+    var w = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth
+    var x = document.getElementById("header1")
+    var y = document.getElementById("header2")
+    var z = document.getElementById("headerPara")
+    var himg = document.getElementById("header_img")
+    var navcon = document.getElementById("brand")
+    var mimg = document.getElementById("main_img")
+
+    if (w <= 576) {
+        x.style.display = "none"
+        x.style.paddingRight = "0"
+        y.style.display = "block"
+        y.style.paddingRight = `${w/2 - 76}px`
+        z.style.display = "none"
+        z.style.paddingRight = "0"
+        navcon.style.display = "block"
+        himg.style.display = "none"
+        if (mimg) {
+            mimg.innerHTML = "<img src='images/Introduction2.png' style='width:100%' />"
+        }
+    }
+
+    else if (w < 1100) {
+        x.style.display = "block"
+        x.style.paddingRight = `${w/2 - 320}px`
+        y.style.display = "none"
+        y.style.paddingRight = "0"
+        z.style.display = "none"
+        z.style.paddingRight = "0"
+        navcon.style.display = "none"
+        himg.innerHTML = "<img src='images/Westlake_Logo_1.png' style='height:3rem' />"
+        himg.style.padding = "0 1rem"
+        himg.style.display = "block"
+        if (mimg) {
+            mimg.innerHTML = "<img src='images/Introduction1.png' style='width:100%' />"
+        }
+    }
+
+    else if (w < 1376) {
+        x.style.display = "block"
+        x.style.paddingRight = `${w/2 - 320}px`
+        y.style.display = "none"
+        y.style.paddingRight = "0"
+        z.style.display = "block"
+        z.style.paddingRight = `${w/2 - 314}px`
+        navcon.style.display = "none"
+        himg.innerHTML = "<img src='images/Westlake_Logo_1.png' style='height:4.38rem' />"
+        himg.style.padding = "0.45rem 1.14rem"
+        himg.style.display = "block"
+        if (mimg) {
+            mimg.innerHTML = "<img src='images/Introduction1.png' style='width:100%' />"
+        }
+    }
+
+    else {
+        x.style.display = "block"
+        x.style.paddingRight = `${w/2 - 320}px`
+        y.style.display = "none"
+        y.style.paddingRight = "0"
+        z.style.display = "block"
+        z.style.paddingRight = `${w/2 - 314}px`
+        navcon.style.display = "none"
+        himg.innerHTML = "<img src='images/Westlake_Logo_0.png' style='height:4.5rem' />"
+        himg.style.padding = "0.4rem 1rem"
+        himg.style.display = "block"
+        if (mimg) {
+            mimg.innerHTML = "<img src='images/Introduction1.png' style='width:100%' />"
+        }
+    }
+}
